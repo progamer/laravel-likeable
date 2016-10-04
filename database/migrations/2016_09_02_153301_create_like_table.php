ab91d4295198c0fedb122fd3d75c13c89ed2fe68
Issa Khalil <issa.mh.khalil@gmail.com>
2016-10-04 22:38:15 +0300

    Update 2016_09_02_153301_create_like_table.php

--- database/migrations/2016_09_02_153301_create_like_table.php
+++ database/migrations/2016_09_02_153301_create_like_table.php
@@ -33,7 +33,7 @@ class CreateLikeTable extends Migration
                 'user_id',
             ], 'like_user_unique');
 
-            $table->foreign('user_id')->references('id')->on('user')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('user')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
